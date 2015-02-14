404572fcb75305a56f0065a78caa7e2e315fe568
Shuji Yamasaki <yamasaki.shuji@gmail.com>
2015-02-14 10:23:54 +0900

    return if frames are undefined.

--- mycdn/javascripts/frame-recur.js
+++ mycdn/javascripts/frame-recur.js
@@ -7,8 +7,12 @@ frameRecur(window, function(f){
 function frameRecur(win, fn){
   fn(win);
   var fs = win.frames;
+  if(!fs)
+    return false;
   for(var i = 0; i < fs.length; ++i){
     var f = fs[i];
     frameRecur(f, fn);
   }
+
+  return true;
 }
