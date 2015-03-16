260c20c5aa39a93801d50e08f3797694a5d27b32
Shuji Yamasaki <yamasaki.shuji@gmail.com>
2015-03-16 15:01:04 +0900

    init

--- index_generator.rb
+++ index_generator.rb
@@ -1,18 +1,11 @@
 #!ruby
 
-def output_dir(argv, html)
-  Dir.foreach(argv) do |f|
-    next if f =~ /^(\.)/
-    p = File.join(argv, f)
-    if FileTest.directory?(p)
-      output_dir(p, html)
-    else
-      html[0] += "<li><a href=\"#{p}\">#{p}</a> - #{FileTest.size(p)} bytes - #{File.mtime(p)}"
-    end
-  end
+html = '<html><body><ul>'
+
+Dir.glob("**/*") do |f|
+    next if f =~ /^(\.|index.html$)/ || FileTest.directory?(f)
+    html += "<li><a href=\"#{f}\">#{f}</a> - #{FileTest.size(f)} bytes - #{File.mtime(f)}"
 end
 
-html = ['<html><body><ul>']
-(ARGV.size == 0 ? ['.'] : ARGV).each {|a| output_dir(a, html)}
-html[0] += '</ul></body></html>'
-File.write('index.html', html[0]);
+html += '</ul></body></html>'
+File.write('index.html', html);
