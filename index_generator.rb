7af6d850ffac5654ce5896c96c6eb04e3fbd267b
Shuji Yamasaki <yamasaki.shuji@gmail.com>
2016-01-30 16:32:42 +0900

    added oscillo-training table

--- index_generator.rb
+++ index_generator.rb
@@ -3,7 +3,7 @@
 html = '<html><body><ul>'
 
 Dir.glob("**/*") do |f|
-    next if f =~ /^(\.|index.html$)/ || FileTest.directory?(f)
+    next if f =~ /^(\.|index.html$|.*_assets\/)/ || FileTest.directory?(f)
     html += "<li><a href=\"#{f}\">#{f}</a> - #{FileTest.size(f)} bytes - #{File.mtime(f)}"
 end
 
