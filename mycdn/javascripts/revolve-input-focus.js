76cbccdc6aeb195ea2e767c41151bc1fe2e062f3
a <b>
2024-07-29 09:21:46 +0900

    included the active element into the inputs selector to start from it. removed underscore.js dependency.

--- mycdn/javascripts/revolve-input-focus.js
+++ mycdn/javascripts/revolve-input-focus.js
@@ -14,8 +14,8 @@ function revolveInputFocus(rev)
     });
   }
 
-  var is = $('input:not([type]),input[type=text],input[type=email],input[type=url],input[type=number],input[type=search],input[type=password],textarea').toArray(); //really nothing such a selector? See a comment of Ron Lussier in http://api.jquery.com/text-selector/.
-  var iof = _.indexOf(is, document.activeElement);
+  var is = $(':focus, input:not([type]),input[type=text],input[type=email],input[type=url],input[type=number],input[type=search],input[type=password],textarea').toArray(); //really nothing such a selector? See a comment of Ron Lussier in http://api.jquery.com/text-selector/.
+  var iof = is.indexOf(document.activeElement);
   set_focus_loop(rev
                  ? is.slice(0, iof).reverse().concat(is.slice(iof + 1).reverse())
                  : is.slice(iof + 1).concat(is.slice(0, iof)));
